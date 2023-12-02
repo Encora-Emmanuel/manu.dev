cc5a9d26aba61da0d0bb3e27094ca0eb205a7f14
Emmanuel Rosa Sarachini de Oliveira <eoliveira95@gmail.com>
2023-12-02 18:02:48 -0300

    inital commit for resume

--- src/hooks/useCurrentFilename.js
+++
@@ -1,6 +0,0 @@
-import { useLocation } from "@reach/router";
-
-export function useCurrentFilename() {
-  const location = useLocation();
-  return location.pathname.split("/").slice(-1)[0] || "README.md";
-}
--- src/utils/mouseFocusSelector.js
+++
@@ -1,6 +0,0 @@
-/**
- * CSS selector to use when you want to style elements that were focused by the mouse and not the keyboard.
- *
- * @see https://css-tricks.com/keyboard-only-focus-styles/
- */
-export const mouseFocusSelector = ":focus:not(:focus-visible)";
